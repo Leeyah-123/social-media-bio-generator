8dba7246f93454286d3f851149029477c5decf58
Leeyah-123 <junaidaaliyah260@gmail.com>
2023-02-12 00:23:12 +0100

    refactor: Refactoring prompt

--- utils/generateBio.ts
+++ utils/generateBio.ts
@@ -28,13 +28,13 @@ export const generateBio = async (
       prompt = `From the following description: "${desc}", generate two funny ${platform} bios clearly labeled 1. and 2. for a person named ${name}.
 
     Make sure to throw in a joke in there and make sure to include each of the following keywords: ${keywords}. Write the bio from a ${pov} point of view and do not include any hashtags.
-    The generated bio should be no less than 500 words. 
+    The generated bio should be no less than 300 words long.
   `;
   } else {
     prompt = `From the following description: "${desc}", generate two ${vibe} ${platform} bios clearly labeled 1. and 2. for a person named ${name}.
 
     Make sure to include each of the following keywords: ${keywords}. Make sure to write the bio from a ${pov} point of view.
-    The generated bio should be no less than 500 words. 
+    The generated bio should be no less than 300 words long. 
   `;
   }
 
